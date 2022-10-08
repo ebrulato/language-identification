4c8c7560bb4395ed7a2b31c47f0c60f1c1c9a03b
ebrutato <1073783+ebrulato@users.noreply.github.com>
2022-10-08 09:57:38 +0200

    Dokerigi

---
+++ Dockerfile
@@ -0,0 +1,21 @@
+# Krei bildon de language-identification
+
+# Por RP4 8Gb komenco
+FROM arm64v8/python:3.10.7-alpine
+RUN apk add --update alpine-sdk
+# Por RP4 8Gb fino
+
+RUN wget https://dl.fbaipublicfiles.com/fasttext/supervised-models/lid.176.bin
+RUN pip install flask
+RUN pip install waitress
+RUN pip install fasttext
+
+# Por RP4 8Gb
+# RUN apk del alpine-sdk
+
+COPY . /app
+WORKDIR /app
+ENV WAITRESS_PORT 3000
+ENV WAITRESS_HOST 0.0.0.0
+EXPOSE $WAITRESS_PORT
+CMD [ "python", "app.py" ]
--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
+import os
+import waitress
 from flask import Flask, request
 from language_identification import LanguageIdentification
 app = Flask(__name__)
@@ -13,6 +15,9 @@ def add_message():
     lang = LANGUAGE.predict_lang(content['text'])
     return {"lang":lang[0][0], "score": lang[1][0]}
 
+def create_app():
+    return app
+
 """
 curl request is as follows
 
@@ -23,5 +28,6 @@ curl -X POST \
 """
 
 if __name__ == '__main__':
-    app.run(host= '127.0.0.1', port= 5000)
+    waitress.serve(create_app(), port=os.environ.get("WAITRESS_PORT"), host=os.environ.get("WAITRESS_HOST"))
+    #app.run(host= '127.0.0.1', port= 5000)
 
